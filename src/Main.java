a5460aaa4714ca35a68b27958c4fbc2b068a7564
Maxim <mqithelp@yandex.ru>
2024-05-12 18:06:02 +0300

    Task 3 is done.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("=====[ Welcome to Task 1 ]=====");
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
         System.out.println("Varable dog: " + dog);
         System.out.println("Varable cat: " + cat);
         System.out.println("Varable paper: " + paper);
+        System.out.println("=====[ Welcome to Task 2 ]=====");
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
         System.out.println("Varable dog: " + dog);
         System.out.println("Varable cat: " + cat);
         System.out.println("Varable paper: " + paper);
+        System.out.println("=====[ Welcome to Task 3 ]=====");
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println("Varable dog: " + dog);
+        System.out.println("Varable cat: " + cat);
+        System.out.println("Varable paper: " + paper);
+
     }
 }
