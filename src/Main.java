21be15a92338d5ee92c5d6c99c5a38106999caa0
Maxim <mqithelp@yandex.ru>
2024-05-12 18:30:37 +0300

    Task 4 is done.

--- src/Main.java
+++ src/Main.java
@@ -23,5 +23,12 @@ public class Main {
         System.out.println("Varable cat: " + cat);
         System.out.println("Varable paper: " + paper);
 
+        System.out.println("=====[ Welcome to Task 4 ]=====");
+        var friend = 19;
+        System.out.println("Initial variable Friend: " + friend);
+        friend = friend +2;
+        System.out.println("Friend increase by 2: " + friend);
+        friend = friend / 7;
+        System.out.println("Friend divide by 7: " + friend);
     }
 }
