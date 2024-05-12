bb2d33f39c644f4cbe73d80bec39e43e3577ccbf
Maxim <mqithelp@yandex.ru>
2024-05-12 17:57:39 +0300

    Task 1 is done.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("Varable dog: " + dog);
+        System.out.println("Varable cat: " + cat);
+        System.out.println("Varable paper: " + paper);
+
     }
 }
